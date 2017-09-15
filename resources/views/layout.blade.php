97925441bb83204812f354d8082e9db5ebdbe0b2
jackyzhang <852566802@qq.com>
2017-09-15 17:06:20 +0800

    init

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<html>
+<title>
+    Web Name - @yield('title')
+</title>
+<body>
+<p2>This is layout!</p2>
+
+@section('sidebar')
+This is main sidebar
+@show
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $data = DB::table('opt_users')->get();
+
+    $client = Elasticsearch\ClientBuilder::create()
+        ->setHosts(['192.168.1.233'])
+        ->build();
+
+//    $params = [
+//        'index' => 'my_index',
+//        'type' => 'my_type',
+//        'id' => 'my_id',
+//        'body' => ['testField' => 'abc']
+//    ];
+//
+//    $response = $client->index($params);
+//    print_r($response);
+
+
+//    $params = [
+//        'index' => 'my_index',
+//        'type' => 'my_type',
+//        'id' => 'my_id'
+//    ];
+//
+//    $response = $client->get($params);
+//    $source = $client->getSource($params);
+//    dump($source);
+//    dump($response);
+
+    $params = [
+        'index' => 'my_index',
+        'type' => 'my_type',
+        'body' => [
+            'query' => [
+                'match' => [
+                    'testField' => 'abc'
+                ]
+            ]
+        ]
+    ];
+
+    $response = $client->search($params);
+    dump($response);
+
+
+    exit;
+
+    $class = new ReflectionClass(App\Http\Controllers\Auth\LoginController::class);
+    $constructor = $class->getConstructor();
+    dump($constructor);
+    $paras = $constructor->getParameters();
+    dump($paras);
+
+
+
+    exit;
+
+    return view('welcome');
+
+    $data = request()->validate([
+        'my' => [new App\Rules\MyCustomRule()],
+        'more_my' => [function ($attribute, $value, $fail) {
+            if ($value <= 10) {
+                $fail(':attribute needs more cowbell!');
+            }
+        }]
+    ]);
+
+    dump($data);
+
+    exit;
+
+
+
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    })
+        ->reject(function ($name) {
+            return empty($name);
+        });
+
+    var_dump($collection->toJson());
+    exit;
+
+
+    return view('test');
+});
